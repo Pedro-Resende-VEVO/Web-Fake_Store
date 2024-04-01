a73ba9edcbd45bce3efce0fc1fcae2f83f437e32
Pedro Henrique Resende Menezes <pedrohrm050315@hotmail.com>
2024-04-01 17:11:19 -0300

    registro Pantry

--- code/register/registro.js
+++ code/register/registro.js
@@ -3,3 +3,62 @@ function loginDirect() {
     window.location.href = "/code/login/login.html";
 }
 
+function realizarRegistro(){
+
+    let inputEma = document.getElementById('inputEma').value;
+    let inputPass = document.getElementById('inputPass').value;
+
+    if(validarRegistro(inputEma, inputPass) == false){
+        console.log("registro inválido")
+    }
+    else{
+        inserirRegistro(inputEma, inputPass);
+    }
+}
+
+function validarRegistro(inputEma, inputPass) {
+    $.ajax({
+        url: 'https://getpantry.cloud/apiv1/pantry/3b2cb07f-5375-46d8-981f-cf128ad64399/basket/Users'
+    })
+        .done(response => {
+            if (inputEma != '' && inputPass != '') {
+                for (let i = 0; i < response.lenght; i++) {
+                    if (inputEma == response[i].email || inputPass == response[i].password) {
+                        return false
+                    }
+                }
+                return true
+            }
+            return false
+        })
+}
+
+function inserirRegistro(email, password) {
+    var settings = {
+        "url": "https://getpantry.cloud/apiv1/pantry/YOUR_PANTRY_ID/basket/YOUR_BASKET_NAME",
+        "method": "PUT",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "2":{
+                "email": email,
+                "password": password
+            }
+        }),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        console.log("Sucess");
+    });
+}
+
+
+$.ajax({
+    url: 'https://getpantry.cloud/apiv1/pantry/3b2cb07f-5375-46d8-981f-cf128ad64399/basket/Users'
+})
+    .done(response => {
+        console.log(response[1].email);
+    })

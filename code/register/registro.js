9efc04ff8b3fe35515d466e5e9b5126d52827de2
Pedro Henrique Resende Menezes <pedrohrm050315@hotmail.com>
2024-04-01 11:07:05 -0300

    remove sqlite3

--- code/register/registro.js
+++ code/register/registro.js
@@ -1,5 +1,11 @@
 
-function loginDirect(){
+function loginDirect() {
     window.location.href = "/code/login/login.html";
 }
 
+
+var db = openDatabase("SesasDB", "1.0", "Mybase", 4048);
+
+db.transaction(function (criar) {
+    criar.executeSql("CREATE TABLE users (id PRIMARY KEY, email TEXT, senha TEXT");
+});
